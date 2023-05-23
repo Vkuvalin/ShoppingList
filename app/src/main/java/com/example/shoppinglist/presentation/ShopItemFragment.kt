36f741e4025738b78d3df2d8e8f8c881c2d1cf10
Кувалин Влад <cuvka000@yandex.ru>
2023-05-23 14:45:11 +0300

    Перехожу к созданию фрагментов. Последующие изменения буду пилить в ветке "framgents".

--- app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.shoppinglist.R
 
-class ShopItemSragment: Fragment() {
+class ShopItemFragment: Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
